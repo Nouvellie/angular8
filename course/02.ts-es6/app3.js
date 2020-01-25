f05a626be269abe787b61af93be9d1ee0fc23117
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-01-24 22:04:43 -0300

    typescript introduction

---
+++ course/02.ts-es6/app3.js
@@ -0,0 +1,21 @@
+let name1;
+name1 = "Roberto";
+let countMinutes;
+countMinutes = 123;
+let confirmation;
+confirmation = true;
+let today;
+today = new Date('2020-06-04');
+let some;
+some = name;
+some = countMinutes;
+some = confirmation;
+some = today;
+let profile = {
+    name2: "Roberto",
+    age: 29
+};
+profile = {
+    name2: "Rocuant",
+    age: 33
+};
---
+++ course/02.ts-es6/app3.ts
@@ -0,0 +1,27 @@
+let name1:string;
+name1 = "Roberto";
+
+let countMinutes:number;
+countMinutes = 123;
+
+let confirmation:boolean;
+confirmation = true;
+
+let today:Date;
+today = new Date('2020-06-04');
+
+let some:any;
+some = name;
+some = countMinutes;
+some = confirmation;
+some = today;
+
+let profile = {
+    name2: "Roberto",
+    age: 29
+}
+
+profile = {
+    name2: "Rocuant",
+    age: 33 
+}
