a593d58f1c7391247f97fe1de0c574e288d22f5a
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-01-24 22:38:57 -0300

    +text

---
+++ course/02.ts-es6/app4.js
@@ -0,0 +1,7 @@
+let firstName = "Roberto";
+let lastName = "Rocuant";
+let age = 29;
+let all1 = "Hellow, " + firstName + " " + lastName + " (" + age + ")";
+let all2 = `Hellow, ${firstName} ${lastName} (${age})`;
+console.log(all1);
+console.log(all2);
---
+++ course/02.ts-es6/app4.ts
@@ -0,0 +1,10 @@
+let firstName:string = "Roberto"
+let lastName:string = "Rocuant"
+let age:number = 29;
+
+let all1 = "Hellow, " + firstName + " " + lastName + " (" + age + ")";
+
+let all2 = `Hellow, ${ firstName } ${ lastName } (${ age })`;
+
+console.log(all1);
+console.log(all2);
