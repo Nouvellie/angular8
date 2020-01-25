6b1943c6fd52c27c15b5a6631dde83d8354f474c
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-01-25 17:07:00 -0300

    ex1 rdy

--- course/02.ts-es6/ex1.ts
+++ course/02.ts-es6/ex1.ts
@@ -1,44 +1,128 @@
 
-// Let y Const Uses.
-var name = "Richard Tapia";
-var age = 23;
+// All the variables will be  Let or Const.
+/*
+[I] As explicit as possible.
+var someName = "Richard Tapia";
+var someAge = 23;
 
 var CHARACTER = {
-  name: name,
-  age: age
-};
+  someName: someName,
+  someAge: someAge
+}; 
+*/
 
+// [I] Result:
 
-// Cree una interfaz que sirva para validar el siguiente objeto
-var batman = {
-  nombre: "Bruno Díaz",
-  artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+let someName:string = "Richard Tapia";
+let someAge:number = 23;
+const CHARACTER:{someName:string, someAge:number} = {
+    someName: someName,
+    someAge: someAge
 }
 
-// Convertir esta funcion a una funcion de flecha
-function resultadoDoble( a, b ){
+console.log(CHARACTER);
+
+// [II] Create an interface that serves to validate the following object.
+/*
+let batman = {
+  jLName: "Bruce Wayne",
+  jLMartialArts: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+}
+*/
+
+// [II] Result:
+
+interface justiceLeague {
+    jLName:string;
+    jLMartialArts:string[];
+}
+
+let batman:justiceLeague = {
+    jLName: "Bruce Wayne",
+    jLMartialArts: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+}
+
+console.log(batman);
+
+
+// [III] Convert this function to an arrow function.
+/*
+function doubleResult(a, b) {
   return (a + b) * 2
 }
+*/
 
-function doubleResult = (a:number, b:number) => a + b;
+// [III] Result:
 
-// Función con parametros obligatorios, opcionales y por defecto
-// donde NOMBRE = obligatorio
-//       PODER  = opcional
-//       ARMA   = por defecto = "arco"
-function getAvenger( nombre, poder, arma ){
-  var mensaje;
-  if( poder ){
-     mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+let doubleResult = (a:number, b:number) => (a + b) * 2;
+
+console.log(doubleResult(18, 16));
+
+// [IV] Functions. (required, default, optional) --> fName: required, fPower: optional, fWeapon: default "Bow".
+/*
+function getInfo( fName, fPower, fWeapon ){
+  let message;
+  if( fPower ){
+     message = fName + " has the power to: " + fPower + " and a weapon: " + fWeapon;
   }else{
-     mensaje = nombre + " tiene un " + poder
+     message = fName + " has a " + fPower;
   }
 };
+*/
 
-// Cree una clase que permita manejar la siguiente estructura
-// La clase se debe de llamar rectangulo,
-// debe de tener dos propiedades:
-//   * base
-//   * altura
-// También un método que calcule el área  =  base * altura,
-// ese método debe de retornar un numero.
+// [IV] Result: 
+
+function getInfo(
+            fName:string,
+            fWeapon:string = "Sword",
+            fPower?:string
+) {
+    let message:string;
+    let message2:string;
+
+    if (fPower) {
+        message = fName + " use " + fPower + " with a " + fWeapon + ".";
+        console.log(message);
+        message2 = `${fName} use ${fPower} with a ${fWeapon}.`
+        console.log(message2);
+    }
+    else {
+        message = fName + " have a " + fWeapon + "."
+        console.log(message);
+        message2 = `${fName} have a ${fWeapon}.`
+        console.log(message2);
+    }
+}
+
+getInfo("Nouvellie", "Knife", "Slash");
+getInfo("Roberto", "Spear");
+
+// [V] Create a class that can handle the following structure.
+/*
+The class must be called a rectangle and must have two properties: (base and height)
+Also a method that calculates the area. (base * height)
+And then return a number.
+*/
+
+// [V] Result:
+
+class Rectangle {
+    base:number;
+    height:number;
+    area:number;
+
+    constructor(base:number, height:number) {
+        this.base = base;
+        this.height = height;
+    }
+    calculateArea():number {
+        this.area = this.base * this.height
+        return this.area
+    }
+}
+
+let rectangle = new Rectangle(3, 5)
+
+console.log(rectangle);
+console.log(rectangle.calculateArea())
+console.log(rectangle.area);
