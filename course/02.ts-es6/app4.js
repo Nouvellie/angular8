7dce39ddaf491d6162766e5afa268d3be498ae15
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-01-24 22:44:33 -0300

    ``abb

--- course/02.ts-es6/app4.js
+++ course/02.ts-es6/app4.js
@@ -3,5 +3,15 @@ let lastName = "Rocuant";
 let age = 29;
 let all1 = "Hellow, " + firstName + " " + lastName + " (" + age + ")";
 let all2 = `Hellow, ${firstName} ${lastName} (${age})`;
+let all3 = `Hellow,
+${firstName} ${lastName}
+(${age})`;
+
+function getName() {
+    return "Roberto Rocuant";
+}
+let all4 = ` ${getName()} `;
 console.log(all1);
 console.log(all2);
+console.log(all3);
+console.log(all4);
--- course/02.ts-es6/app4.ts
+++ course/02.ts-es6/app4.ts
@@ -6,5 +6,16 @@ let all1 = "Hellow, " + firstName + " " + lastName + " (" + age + ")";
 
 let all2 = `Hellow, ${ firstName } ${ lastName } (${ age })`;
 
+let all3 = `Hellow,
+${ firstName } ${ lastName }
+(${ age })`;
+
+function getName() {
+    return "Roberto Rocuant";
+}
+
+let all4:string = ` ${ getName() } `;
 console.log(all1);
 console.log(all2);
+console.log(all3);
+console.log(all4);
