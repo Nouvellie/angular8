16e5efdfb5714f88d5ee6d4b3e2d218695e72586
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-01-26 04:04:14 -0300

    heroe slug

--- course/04.secondapp/src/app/components/heroe/heroe.component.ts
+++ course/04.secondapp/src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Heroe, HeroesService } from '../../services/heroes.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe:Heroe;
 
-  ngOnInit() {
-  }
+  constructor( private _activateRoute:ActivatedRoute,
+               private _heroesService:HeroesService
+            ) {
+    this._activateRoute.params.subscribe( params => {
+      console.log(params.id);
+      this.heroe = this._heroesService.getHeroe(params.id);
+    })
+   }
+
 
 }
