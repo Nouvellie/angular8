1d6801c10ddfd6bc39ef8e5d9f98d14a5ff52408
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-01-25 22:11:46 -0300

    body and header component added

---
+++ course/03.firstapp/src/app/components/body/body.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component ({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+export class BodyComponent {
+
+}
--- course/03.firstapp/src/app/components/header/header.component.ts
+++ course/03.firstapp/src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'app-header',
-    template: `<h1>Header Component</h1>`
+    templateUrl: './header.component.html'
 })
 export class HeaderComponent {
     
