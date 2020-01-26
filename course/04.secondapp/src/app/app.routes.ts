800a80f6a372e52886e0406537aaecb0ef495f7c
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-01-26 02:00:36 -0300

    router link active

--- course/04.secondapp/src/app/app.routes.ts
+++ course/04.secondapp/src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { RouterModule, Routes, Route } from '@angular/router';
+
+// Components:
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 import { HomeComponent } from './components/home/home.component';
 
+
+
 const APP_ROUTES:Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: '', component: HomeComponent },
     { path: '**', pathMatch: 'full', redirectTo: ''}
 ]
 
