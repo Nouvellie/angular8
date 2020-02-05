6a8d46bc1d6e168f6c7452a1496d6722253d6c43
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-02-05 00:39:15 -0300

    noimage pipe ready

--- course/06.fourthapp/src/app/app.module.ts
+++ course/06.fourthapp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { NoimagePipe } from './pipes/noimage.pipe';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     HomeComponent,
     SearchComponent,
     ArtistComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     BrowserModule,
