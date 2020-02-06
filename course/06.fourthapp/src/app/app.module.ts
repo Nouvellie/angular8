64b3f8a1258ac2cfd12f797572045f1938d4332a
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-02-06 01:05:32 -0300

    safedom fixed

--- course/06.fourthapp/src/app/app.module.ts
+++ course/06.fourthapp/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { NoimagePipe } from './pipes/noimage.pipe';
-import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { SafedomPipe } from './pipes/safedom.pipe';
 import { CardsComponent } from './components/cards/cards.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
 
@@ -27,7 +27,7 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     NoimagePipe,
     CardsComponent,
     LoadingComponent,
-    DomseguroPipe,
+    SafedomPipe,
   ],
   imports: [
     BrowserModule,
