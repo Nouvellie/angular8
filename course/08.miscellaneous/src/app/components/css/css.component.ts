14d455a66347221e4fa72f63d3dd3a15f1fe287a
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-02-11 23:05:58 -0300

    ng style

---
+++ course/08.miscellaneous/src/app/components/css/css.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css',
+  template: `
+    <p>
+      Css p. 
+    </p>
+  `,
+  styles: []
+})
+export class CssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
