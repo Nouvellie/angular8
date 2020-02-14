4db7ae5d1e31b81bcbd6241198d4b99bf744a4ba
Cardionomous Team <roberto.rocuantv@gmail.com>
2020-02-13 23:13:57 -0300

    ng class

--- course/08.miscellaneous/src/app/components/classes/classes.component.ts
+++ course/08.miscellaneous/src/app/components/classes/classes.component.ts
@@ -3,14 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-classes',
   template: `
-    <div class="alert alert-success" role="alert">
+    <!-- <div [ngClass]="'alert-info'" class="alert" role="alert"> -->
+    <div [ngClass]="alert" class="alert" role="alert">
       A simple success alert—check it out!
     </div>
+    <button class="btn btn-info" type="button" (click)="alert = 'alert-info'">Info</button>&nbsp;
+    <button class="btn btn-success" type="button" (click)="alert = 'alert-success'">Success</button>
+
+    <br> <br>
+    
+    <h3 [ngClass]="{'text-danger': properties.danger, 'text-info': !properties.danger }">
+      Hellow World!
+    </h3>
+
+    <button [ngClass]="{'btn-danger': properties.danger, 'btn-info': !properties.danger }" (click)="properties.danger = !properties.danger" class="btn" type="button" name="button">
+      Danger - Info
+    </button>
   `,
   styles: []
 })
 export class ClassesComponent implements OnInit {
 
+  alert:string = "alert-danger"
+
+  properties:Object = {
+    danger: false,
+  }
+  
   constructor() { }
 
   ngOnInit() {
